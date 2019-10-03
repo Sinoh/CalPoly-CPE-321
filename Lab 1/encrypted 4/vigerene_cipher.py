ed7d671fd7ba99031db90b2701dbae0264e344da
Jeffery Ho <jho74@calpoly.edu>
2019-10-02 21:33:53 -0700

    Update vigerene_cipher.py

--- Lab 1/encrypted 4/vigerene_cipher.py
+++ Lab 1/encrypted 4/vigerene_cipher.py
@@ -71,7 +71,7 @@ def main():
                 decrypt_output.write(decrypt(string, key) + '\n')
                 count += 1
                 percent = count/pow(26, key_length) * 100
-                if (percent % 1):
+                if (percent % 1 == 0):
                     print('%0.2f' %(percent), '%')
 
                 
